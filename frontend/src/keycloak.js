5b97785209e47f297220705c690815d2eaa24ad5
s-quatres <simon.quatresols@gmail.com>
2025-10-31 15:10:38 +0100

    Fix keycloak issues

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,13 +1,14 @@
 import Keycloak from "keycloak-js";
 
 // Get config from runtime (injected by docker-entrypoint.sh) or build-time env vars
-const runtimeConfig = window.RUNTIME_CONFIG || {};
+const runtimeConfig =
+  (typeof window !== "undefined" && window.RUNTIME_CONFIG) || {};
 
 const keycloakConfig = {
   url:
     runtimeConfig.KEYCLOAK_URL ||
     import.meta.env.VITE_KEYCLOAK_URL ||
-    "http://localhost:8080",
+    "https://keycloak.quatres.net",
   realm:
     runtimeConfig.KEYCLOAK_REALM ||
     import.meta.env.VITE_KEYCLOAK_REALM ||
@@ -17,6 +18,8 @@ const keycloakConfig = {
     import.meta.env.VITE_KEYCLOAK_CLIENT_ID ||
     "qjournal-app",
 };
+
+console.log("Keycloak config:", keycloakConfig);
 
 const keycloak = new Keycloak(keycloakConfig);
 
