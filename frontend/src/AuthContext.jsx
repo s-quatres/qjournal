a1ece6251e4ec48eea0e43cecc969b1ec7987cce
s-quatres <simon.quatresols@gmail.com>
2025-11-01 12:06:52 +0100

    Remove PKCE to test token exchange

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -100,7 +100,6 @@ export const AuthProvider = ({ children }) => {
         onLoad: "check-sso",
         checkLoginIframe: false,
         responseMode: "fragment",
-        pkceMethod: "S256",
       }}
       onEvent={handleEvent}
       onTokens={handleTokens}
